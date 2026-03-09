8db573d067e1aa61f555b6bb326a1cedbdfc2a21
adriankrawczyk <adriankrawczyk0@o2.pl>
2026-03-09 14:06:52 +0100

    Tutorial 1

--- App.tsx
+++ App.tsx
@@ -1,25 +1,34 @@
 import { StatusBar } from 'expo-status-bar'
 import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native'
-import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
+import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming, withRepeat, SharedValue } from 'react-native-reanimated'
 
 const SIZE = 100;
+
+const handleRotation = (progress: SharedValue<number>) => {
+  'worklet';
+  return `${progress.value * 2 * Math.PI}rad`;
+};
 
 export default function App() { 
 
   const progress = useSharedValue(0);
+  const scale = useSharedValue(1);
 
   const reanimatedStyle = useAnimatedStyle(() => {
     return {
       opacity: progress.value,
+      borderRadius: progress.value * SIZE / 2,
       height: SIZE,
       width: SIZE,
       backgroundColor: 'blue',
-    };
+      transform: [{ scale: scale.value, }, {rotate: handleRotation(progress) }]
+    }; 
   }, []);
 
   useEffect(() => {
-    progress.value = withTiming(1, { duration: 1000 });
+    progress.value = withRepeat(withSpring(1, { duration: 1000 }), -1, true);
+    scale.value = withRepeat(withSpring(2, { duration: 1000 }), -1, true);
   }, []);
 
   return (  
