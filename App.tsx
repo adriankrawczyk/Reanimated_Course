8e0247b2bcb6afd8f30f697393f06cb71ad634fc
adriankrawczyk <adriankrawczyk0@o2.pl>
2026-03-09 13:38:40 +0100

    Blue square

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
 
-export default function App() {
-  return (
+const SIZE = 100;
+
+export default function App() { 
+
+  const progress = useSharedValue(1);
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      height: SIZE,
+      width: SIZE,
+      backgroundColor: 'blue',
+    };
+  }, []);
+
+  return (  
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={reanimatedStyle} />
     </View>
   )
 }
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 export default function (api) {
   api.cache(true)
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin'
+    ],
   }
 }
